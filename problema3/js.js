aa89b864e0817ede748637bf17a64cdcf3014af4
Sebastián Peraza <seperazade@est.utn.ac.cr>
2024-03-20 19:12:36 -0600

    Delete problema3 - era problema 2
    el nombre del ejercicio era "Problema2"

--- problema3/js.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.ima').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.imagenG').src = item.src; // Cambia la imagen grande.
-        document.getElementById('imageTitle').textContent = item.alt; // Cambia el título.
-        document.getElementById('imageDescription').textContent = item.getAttribute('data-description'); // Cambia la descripción.
-    });
-});
